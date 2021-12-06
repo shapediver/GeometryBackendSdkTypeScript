c012ea8b34180668603a6fda84c91c3edb28e2cd
Matthias Reisacher <matt@shapediver.com>
2021-12-06 09:34:34 +0100

    Expose ShapeDiver error types

--- packages/sdk.geometry-api-sdk-v2/src/index.ts
+++ packages/sdk.geometry-api-sdk-v2/src/index.ts
@@ -1,2 +1,7 @@
 export * from "@shapediver/api.geometry-api-dto-v2"
+export {
+    ShapeDiverError,
+    ShapeDiverRequestError,
+    ShapeDiverResponseError,
+} from "@shapediver/sdk.geometry-api-sdk-core"
 export { create, ShapeDiverSdk } from "./ShapeDiverSdk"
