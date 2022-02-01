616d491fb509392e157246d35ae0b6469ea52577
Matthias Reisacher <matt@shapediver.com>
2022-02-01 14:19:37 +0100

    Extend analytics-test

--- packages/sdk.geometry-api-sdk-v2/__tests__/analyticsApi.test.ts
+++ packages/sdk.geometry-api-sdk-v2/__tests__/analyticsApi.test.ts
@@ -15,14 +15,25 @@ describe("analytics Api", () => {
 
     it("model session statistics", async () => {
         const res = await sdk.analytics.modelSessionStatistics({
-            "parameters": [ {
-                "modelid": getTestModel1(),
-                "timestamp": "2020",
-            } ],
+            "parameters": [
+                {
+                    "modelid": getTestModel1(),
+                    "timestamp": "202101",
+                },
+                {
+                    "modelid": getTestModel1(),
+                    "timestamp": [ "202112", "202101" ],
+                },
+                {
+                    "modelid": getTestModel1(),
+                    "timestamp_from": "202012",
+                    "timestamp_to": "202101",
+                },
+            ],
         })
         expect(res).toBeDefined()
         expect(res.analytics).toBeDefined()
-        expect(res.analytics!.models.length).toBe(1)
+        expect(res.analytics!.models.length).toBe(3)
     })
 
 })
