56e59c000cc81e9c7c47381b88810472b1c57745
Matthias Reisacher <matt@shapediver.com>
2025-07-28 11:33:21 +0200

    SS-8849 Fix jwt issue in init-session via ticket

--- packages/sdk.geometry-api-sdk-v2/__tests__/base.test.ts
+++ packages/sdk.geometry-api-sdk-v2/__tests__/base.test.ts
@@ -21,74 +21,114 @@ describe('constructor', function () {
     });
 });
 
-describe('inceptor', function () {
+describe('axios-mock', function () {
     const config = new Configuration({ useCustomAxios: true, maxRetries: 2 }),
         api = new SessionApi(config), // use actual api instance to test BaseAPI swapping
         mock = new AxiosMockAdapter(api.defaultAxios!);
-    let spyPost: number;
 
     beforeEach(() => {
         mock.resetHandlers();
-        spyPost = 0;
     });
 
-    test('retry enabled, general error status; should not retry', async () => {
-        mock.onPost().reply(() => {
-            spyPost++;
-            return [400, {}];
+    afterAll(() => {
+        mock.restore();
+    });
+
+    describe('inceptor', function () {
+        let spyPost: number;
+
+        beforeEach(() => {
+            spyPost = 0;
         });
 
-        await expect(api.createSessionByTicket('foobar')).rejects.toThrow(
-            /Request failed with status code 400/
-        );
-        expect(spyPost).toBe(1);
-    });
-
-    test.each([
-        [429, { 'retry-after': '1' }],
-        [502, {}],
-    ])(
-        'retry enabled, status code %s; should retry until failure',
-        async (statusCode, headers) => {
+        test('retry enabled, general error status; should not retry', async () => {
             mock.onPost().reply(() => {
                 spyPost++;
-                return [statusCode, {}, headers];
+                return [400, {}];
             });
 
             await expect(api.createSessionByTicket('foobar')).rejects.toThrow(
-                new RegExp(`Request failed with status code ${statusCode}`)
+                /Request failed with status code 400/
             );
-            expect(spyPost).toBe(3);
-        }
-    );
-
-    test.each([
-        [429, { 'retry-after': '1' }],
-        [502, {}],
-    ])('retry enabled, status code %s once; should retry once', async (statusCode, headers) => {
-        mock.onPost().reply(() => {
-            if (spyPost++ === 0) return [statusCode, {}, headers];
-            else return [200, {}];
+            expect(spyPost).toBe(1);
         });
 
-        await expect(api.createSessionByTicket('foobar')).resolves.toBeDefined();
-        expect(spyPost).toBe(2);
+        test.each([
+            [429, { 'retry-after': '1' }],
+            [502, {}],
+        ])(
+            'retry enabled, status code %s; should retry until failure',
+            async (statusCode, headers) => {
+                mock.onPost().reply(() => {
+                    spyPost++;
+                    return [statusCode, {}, headers];
+                });
+
+                await expect(api.createSessionByTicket('foobar')).rejects.toThrow(
+                    new RegExp(`Request failed with status code ${statusCode}`)
+                );
+                expect(spyPost).toBe(3);
+            }
+        );
+
+        test.each([
+            [429, { 'retry-after': '1' }],
+            [502, {}],
+        ])(
+            'retry enabled, status code %s once; should retry once',
+            async (statusCode, headers) => {
+                mock.onPost().reply(() => {
+                    if (spyPost++ === 0) return [statusCode, {}, headers];
+                    else return [200, {}];
+                });
+
+                await expect(api.createSessionByTicket('foobar')).resolves.toBeDefined();
+                expect(spyPost).toBe(2);
+            }
+        );
+
+        test('retry disabled, retry-able error status; should not retry', async () => {
+            const api = new SessionApi(new Configuration({ useCustomAxios: false })),
+                mock = new AxiosMockAdapter(axios); // Mock global Axios
+
+            mock.onPost().reply(() => {
+                spyPost++;
+                return [429, {}];
+            });
+
+            await expect(api.createSessionByTicket('foobar')).rejects.toThrow(
+                /Request failed with status code 429/
+            );
+            expect(spyPost).toBe(1);
+        });
     });
 
-    test('retry disabled, retry-able error status; should not retry', async () => {
-        const api = new SessionApi(new Configuration({ useCustomAxios: false })),
-            mock = new AxiosMockAdapter(axios); // Mock global Axios
+    describe('authorization', () => {
+        const ticket = 'some-example-ticket';
 
-        mock.onPost().reply(() => {
-            spyPost++;
-            return [429, {}];
+        test('no-auth', async () => {
+            const config = new Configuration({});
+
+            mock.onPost().reply((config) => {
+                expect(config.headers).toBeDefined();
+                expect(config.headers!['Authorization']).toBeUndefined();
+                return [200, {}];
+            });
+
+            await new SessionApi(config).createSessionByTicket(ticket);
         });
 
-        await expect(api.createSessionByTicket('foobar')).rejects.toThrow(
-            /Request failed with status code 429/
-        );
-        expect(spyPost).toBe(1);
+        test('jwt', async () => {
+            const jwt = 'some-jwt',
+                config = new Configuration({ accessToken: jwt });
 
-        mock.restore();
+            mock.onPost().reply((config) => {
+                expect(config.headers).toBeDefined();
+                expect(config.headers!['Authorization']).toMatch(new RegExp(`^Bearer ${jwt}`));
+                return [200, {}];
+            });
+
+            await new SessionApi(config).createSessionByTicket(ticket);
+        });
     });
 });
